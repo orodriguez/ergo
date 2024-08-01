eb23d4b77ff7427139113b78623978bb3fde6468
“orodriguez” <omarjavier15@gmail.com>
2024-08-01 12:44:27 -0400

    Configure CORS

--- back_end/config/initializers/cors.rb
+++ back_end/config/initializers/cors.rb
@@ -5,12 +5,13 @@
 
 # Read more: https://github.com/cyu/rack-cors
 
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
-#   allow do
-#     origins "example.com"
-#
-#     resource "*",
-#       headers: :any,
-#       methods: [:get, :post, :put, :patch, :delete, :options, :head]
-#   end
-# end
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins 'http://localhost:3001'  # Replace with your front-end URL or '*' to allow all origins
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      expose: ['Authorization'], # Optional: if you need to expose certain headers
+      credentials: true # Optional: if you need to support credentials
+  end
+end
