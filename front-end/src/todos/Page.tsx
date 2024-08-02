c82a91031cc506fc6e2949647dec55cc25cca425
“orodriguez” <omarjavier15@gmail.com>
2024-08-02 10:59:11 -0400

    * Set focus to new todo on render * handleAddTodo * handleKeyDown

--- front-end/src/todos/Page.tsx
+++ front-end/src/todos/Page.tsx
@@ -1,9 +1,20 @@
 import { Button, TextField, Typography } from "@mui/material";
 import { Box, Container } from "@mui/system";
-import React from "react"
+import React, { useEffect } from "react"
 
 const Page: React.FC = () => {
     const [newTodoTitle, setNewTodoTitle] = React.useState<string>("");
+    const newTodoInputRef = React.useRef<HTMLButtonElement>(null);
+    useEffect(() => newTodoInputRef.current?.focus(), []);
+
+    const handleAddTodo = () => {
+        console.log(`Adding todo: ${newTodoTitle}`);
+        setNewTodoTitle("");
+    };
+
+    const handleKeyDown = (e: React.KeyboardEvent) => {
+        if (e.key === "Enter") handleAddTodo();
+    };
 
     return (
         <Container>
@@ -14,8 +25,11 @@ const Page: React.FC = () => {
                     variant="standard"
                     value={newTodoTitle}
                     onChange={e => setNewTodoTitle(e.target.value)}
+                    onKeyDown={handleKeyDown}
+                    inputRef={newTodoInputRef}
+                    sx={{ width: '40%' }}
                 />
-                <Button variant="contained">Add Todo</Button>
+                <Button variant="contained" onClick={handleAddTodo}>Add Todo</Button>
             </Box>
         </Container>
     )
