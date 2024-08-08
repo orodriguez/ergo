67c61ac0f9a6b47f3c49443bcdcf6508c65f9279
“orodriguez” <omarjavier15@gmail.com>
2024-08-08 19:43:07 -0400

    Config more things on jest

--- front-end/jest.config.js
+++ front-end/jest.config.js
@@ -1,14 +1,15 @@
+console.log('Running jest.config.js');
 // jest.config.js
 module.exports = {
   transform: {
     '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
   },
   transformIgnorePatterns: [
-    '/node_modules/(?!(axios|msw)/)' // Add other packages if needed
+    '/node_modules/(?!(axios|@bundled-es-modules)/)' // Add other packages if needed
   ],
   testEnvironment: 'jsdom',
   moduleNameMapper: {
     '^src/(.*)$': '<rootDir>/src/$1',
-  },
+  }
   // Other Jest configurations...
 };
