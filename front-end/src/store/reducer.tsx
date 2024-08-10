6c536e2ac4a6ce17b47f6f3ea109dd6e802947bb
“orodriguez” <omarjavier15@gmail.com>
2024-08-10 16:25:54 -0400

    Use constants in reducer

--- front-end/src/store/reducer.tsx
+++ front-end/src/store/reducer.tsx
@@ -1,28 +1,26 @@
-import { Action } from "src/todos/actions";
+import * as actions from "src/todos/actions";
 import { State } from "./state";
 
-export const reducer = (state: State, action: Action): State => {
+export const reducer = (state: State, action: actions.Action): State => {
     switch (action.type) {
-        case 'SET_NEW_TODO_TITLE':
+        case actions.NEW_TODO_TITLE_CHANGE:
             return { ...state, newTodoTitle: action.payload };
-        case 'SET_ACTIVE_REQUESTS':
-            return { ...state, activeRequests: action.payload };
-        case 'SET_ITEMS':
+        case actions.FETCH_ALL_TODOS:
             return { ...state, todos: action.payload };
-        case 'ADD_TODO':
+        case actions.ADD_TODO:
             return { ...state, todos: [action.payload, ...state.todos], newTodoTitle: '' };
-        case 'UPDATE_ITEM':
+        case actions.TOGGLE_TODO_COMPLETED:
             return {
                 ...state,
                 todos: state.todos.map(item =>
                     item.id === action.payload.id ? { ...item, completed: action.payload.completed } : item
                 ),
             };
-        case 'REMOVE_ITEM':
+        case actions.REMOVE_TODO:
             return { ...state, todos: state.todos.filter(item => item.id !== action.payload) };
-        case 'SHOW_DELETE_CONFIRMATION':
+        case actions.SHOW_DELETE_CONFIRMATION:
             return { ...state, deleteConfirmationTarget: action.payload };
-        case 'HIDE_DELETE_CONFIRMATION':
+        case actions.HIDE_DELETE_CONFIRMATION:
             return { ...state, deleteConfirmationTarget: null };
         default:
             return state;
