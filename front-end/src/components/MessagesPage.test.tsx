873ac632c28e6e5a4366cf7ba9831ed2fbae42f8
“orodriguez” <omarjavier15@gmail.com>
2024-08-13 10:28:04 -0400

    Add a simple uni test to check that all dependencies work

---
+++ front-end/src/components/MessagesPage.test.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { http, HttpResponse } from 'msw'
+import { setupServer } from 'msw/node'
+import { render, fireEvent, screen, waitFor } from '@testing-library/react'
+import '@testing-library/jest-dom'
+import MessagesPage from './MessagesPage'
+
+const server = setupServer(
+    http.get('http://localhost:5047/messages', () => {
+        return HttpResponse.json([
+            { id: 1, content: 'Message 1' },
+            { id: 2, content: 'Message 2' },
+        ])
+    }),
+)
+
+beforeAll(() => server.listen())
+afterEach(() => server.resetHandlers())
+afterAll(() => server.close())
+
+test('loads and displays greeting', async () => {
+    render(<MessagesPage />)
+
+    screen.getByRole('heading', { name: 'Messages' });
+
+    const items = await screen.findAllByRole('listitem');
+    const messages = items.map(item => item.textContent);
+
+    expect(messages).toEqual(['Message 1', 'Message 2']);
+})
