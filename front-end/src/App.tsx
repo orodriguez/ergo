e60d0e1e39fd066b881d1052ee9186f6c6262954
“orodriguez” <omarjavier15@gmail.com>
2024-08-13 11:35:51 -0400

    Install react router

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -2,15 +2,22 @@ import React from 'react';
 import { Container, CssBaseline, ThemeProvider } from '@mui/material';
 import theme from './theme';
 import Messages from './components/MessagesPage';
+import { BrowserRouter, Route, Router, Routes } from 'react-router-dom';
 
 const App: React.FC = () => {
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <Container>
-        <Messages />
-      </Container>
-    </ThemeProvider>
+    <React.StrictMode>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Container>
+          <BrowserRouter>
+            <Routes>
+              <Route path="/messages" element={<Messages />} />
+            </Routes>
+          </BrowserRouter>
+        </Container>
+      </ThemeProvider>
+    </React.StrictMode>
   );
 };
 
