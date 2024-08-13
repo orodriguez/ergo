fe5bfc46c59f12e452806dc0f18eeee6f57ad202
“orodriguez” <omarjavier15@gmail.com>
2024-08-13 10:13:39 -0400

    Handle message with a type and key for iteration

--- front-end/src/components/MessagesPage.tsx
+++ front-end/src/components/MessagesPage.tsx
@@ -2,8 +2,13 @@ import axios from "axios";
 import React, { useEffect } from "react";
 import { useState } from "react";
 
+export type Message = {
+    id: number;
+    content: string;
+}
+
 export const Container: React.FC = (() => {
-    const [messages, setMessages] = useState<string[]>([]);
+    const [messages, setMessages] = useState<Message[]>([]);
 
     useEffect(() => {
         axios.get("http://localhost:5047/messages")
@@ -13,13 +18,13 @@ export const Container: React.FC = (() => {
     return <MessagesPage value={messages} />
 });
 
-export const MessagesPage = ({ value }: { value: string[] }) =>
+export const MessagesPage = ({ value }: { value: Message[] }) =>
     <>
         <h1>Messages</h1>
         {value.length === 0 && <span>...Loading</span>}
         {value.length > 0 && (
             <ul>
-                {value.map(messages => <li>{messages}</li>)}
+                {value.map(message => <li key={message.id}>{message.content}</li>)}
             </ul>
         )}
     </>;
