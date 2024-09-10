26a5cbcbbda7a637ee4e143dcb09cacb13e4e108
“orodriguez” <omarjavier15@gmail.com>
2024-09-10 14:33:12 -0400

    Handle new todo change and new todo key down

--- front-end/src/components/todos/Page.tsx
+++ front-end/src/components/todos/Page.tsx
@@ -1,19 +1,41 @@
 import { Container, TextField } from "@mui/material";
+import { useState } from "react";
 
 const Wrapper: React.FC = () => {
-    const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        console.log(e.target.value);
+    const [newTodo, setNewTodo] = useState<string>('');
+
+    const handleNewTodoChange = (e: React.ChangeEvent<HTMLInputElement>) => setNewTodo(e.target.value);
+
+    const handleNewTodoKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+        if (e.key !== 'Enter') return;
+
+        setNewTodo('');
     };
 
-    return <Page onChange={onChange} />;
+    return <Page
+        newTodo={newTodo}
+        onNewTodoChange={handleNewTodoChange}
+        onNewTodoKeyDown={handleNewTodoKeyDown} />;
 };
 
 interface IProps {
-    onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+    newTodo: string;
+    onNewTodoChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+    onNewTodoKeyDown: (e: React.KeyboardEvent<HTMLInputElement>) => void;
 }
 
-const Page: React.FC<IProps> = ({ onChange }: IProps) => <Container sx={{ paddingTop: 3 }}>
-    <TextField label="Add Todo" variant="outlined" onChange={onChange} />
-</Container>;
+const Page: React.FC<IProps> = ({
+    newTodo,
+    onNewTodoChange,
+    onNewTodoKeyDown
+}: IProps) =>
+    <Container sx={{ paddingTop: 3 }}>
+        <TextField
+            label="Add Todo"
+            variant="outlined"
+            value={newTodo}
+            onChange={onNewTodoChange}
+            onKeyDown={onNewTodoKeyDown} />
+    </Container>;
 
 export default Wrapper;
