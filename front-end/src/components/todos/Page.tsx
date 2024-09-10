77aaa55b617844bc4874b9a7143b0dd660267c27
“orodriguez” <omarjavier15@gmail.com>
2024-09-10 14:20:28 -0400

    Add TextField

--- front-end/src/components/todos/Page.tsx
+++ front-end/src/components/todos/Page.tsx
@@ -1,3 +1,19 @@
-const Page: React.FC = () => <h1>Todo Page</h1>;
+import { Container, TextField } from "@mui/material";
 
-export default Page;
+const Wrapper: React.FC = () => {
+    const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        console.log(e.target.value);
+    };
+
+    return <Page onChange={onChange} />;
+};
+
+interface IProps {
+    onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+const Page: React.FC<IProps> = ({ onChange }: IProps) => <Container sx={{ paddingTop: 3 }}>
+    <TextField label="Add Todo" variant="outlined" onChange={onChange} />
+</Container>;
+
+export default Wrapper;
