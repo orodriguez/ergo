96392b427b6e93566f4fbf471d66bff3f3952d7a
“orodriguez” <omarjavier15@gmail.com>
2024-09-10 11:18:54 -0400

    Remove all

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import { CssBaseline, ThemeProvider } from '@mui/material';
-import Todos from './todos';
 import theme from './theme';
 
 const App: React.FC = () => {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <Todos.Page />
+      <div>App</div>
     </ThemeProvider>
   );
 };
--- front-end/src/mocks/handlers.ts
+++
@@ -1,9 +0,0 @@
-// src/mocks/handlers.js
-import { http, HttpResponse } from 'msw'
-
-export const handlers = [
-    http.get('http://localhost:3000/todos', () => {
-        const todos = [{ id: 1, title: 'First todo', completed: false }]
-        return HttpResponse.json(todos);
-    }),
-]
